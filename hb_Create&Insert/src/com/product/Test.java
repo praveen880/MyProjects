729a884f3a096f198691e94b9ad2f73eba161dc8
Induja <Induja@PRAVEEN>
2019-03-16 22:25:07 +0530

    iam committed

---
+++ hb_Create&Insert/src/com/product/Test.java
@@ -0,0 +1,34 @@
+package com.product;
+
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Configuration cfg=new Configuration();
+		cfg.configure("product.cfg.xml");
+		SessionFactory sf=cfg.buildSessionFactory();
+		Session s=sf.openSession();
+		Transaction tx=s.beginTransaction();
+		Product p1=new Product();
+		p1.setPid(1001);
+		p1.setPname("mobile");
+		p1.setPcost(15000);
+		s.save(p1);
+		System.out.println("Obect Saved Successfully");
+		tx.commit();
+		s.close();
+		sf.close();
+		
+
+	}
+
+}
